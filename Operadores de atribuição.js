11ec1fde049babece80a0d15daf019606550ee3d
walterdyna <alexdyna@gmail.com>
2023-04-19 10:31:37 -0300

    Operadores de Atribuição

---
+++ Operadores de atribuição.js
@@ -0,0 +1,48 @@
+//Operadores de atribuição
+
+// =  -> Atribuição
+// += -> Adição
+// -= -> Subtração
+// /= -> Divisão
+// *= -> Multiplicação
+// %= -> resto
+
+// ------====Atribuição====------
+
+//Informa o valor a ser Atribuição a ação:
+let Atribuição = 10
+console.log(Atribuição)
+console.log("-----------------------------------")
+
+// ------====Adição====------
+let Adição = 10 // modo de atribuição
+Adição = Adição + 2 // modo normal
+Adição +=2 // modo mais rapido com a mesma função---- Adiciona mais 2 a função 10 + 2
+
+console.log(Adição)
+console.log("-----------------------------------")
+
+// ------====Subtração====------
+let Subtração = 10 // modo de atribuição
+Subtração = Subtração - 2 // modo normal
+Subtração -=2 // modo mais rapido com a mesma função --- Retira 2 a função 10 - 2
+
+console.log(Subtração)
+console.log("-----------------------------------")
+
+// ------====Divisão====------
+let Divisão = 10 // modo de atribuição
+Divisão = Divisão / 2 // modo normal
+Divisão /=2 // modo mais rapido com a mesma função --- mostra o resto da função no caso de 10 / 2 nao a reste entao zero...
+
+console.log(Divisão)
+console.log("-----------------------------------")
+
+// ------====Multiplicação====------
+let Multiplicação = 10 // modo de atribuição
+Multiplicação = Multiplicação * 2 // modo normal
+Multiplicação *=2 // modo mais rapido com a mesma função --- mostra o resultado de 10 * 2
+
+console.log(Multiplicação)
+console.log("-----------------------------------")
+
---
+++ README.md
@@ -0,0 +1,32 @@
+# Operadores-de-Atribuição - JavaScript
+
+Ex: de Operações.
+let operador = 10
+
+------==== Operadores de atribuição ====------
+
+  = igual --> Atribuição    --> Operdor normal de atribuição :
+   let atribuição = 10 
+
+----------------------------------------------------------------------
+  += Mais igual -> Adição   --> Operdor de Adição :
+  operador += 2     --> Operdor que adciona mais 2 ao operador 10 + 2
+
+----------------------------------------------------------------------
+  -= menor igual -> Subtração  --> Operador de Subtração:
+  operador -= 2     --> Operdor que remove 2 ao operador 10 - 2
+
+----------------------------------------------------------------------
+
+  /= dividir igual -> Divisão  --> Operador de Divisão:
+  operador /= 2     --> Divide o valor da operação por 2 --- 10 / 2
+
+----------------------------------------------------------------------
+  *= multiplica igual -> Multiplicação --> Operador de Multiplicação:
+  operador *= 2     --> multiplica o valor da operação por 2 --- 10 * 2
+
+----------------------------------------------------------------------  
+  %= -> resto igual -> resto --> Operador de resto
+  operador %= 2     -->divide o valor do operador por 2 e mostra o resto da 
+  operação, no cado de 10 % 2 nao a resto, desta forma mostra Zero. 
+  No caso de 10 % 3 sobraria 1
